3bd66b07400bbdd99594abd809c88245e6a78519
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-24 11:50:37 +0530

    employers display

--- fund/employers.php
+++ fund/employers.php
@@ -95,7 +95,7 @@ require_once '../scripts/connection.php';
                 </thead>
                 <tbody>
 				<?php 
-$stmt = $conn->prepare("SELECT * FROM `tblemployers1`");
+$stmt = $conn->prepare("SELECT * FROM `tblemployers`");
 
 $stmt->execute();
 $result = $stmt->get_result();
@@ -160,8 +160,17 @@ while($row = $result->fetch_assoc()) {
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <!-- Vendor JS Files -->
+  <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
 
 
   <!-- Template Main JS File -->
