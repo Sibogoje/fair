e41d73d3b5876740e5515ce737512aa0cb003f22
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-26 16:32:02 +0530

    dd

--- membership/consolsummary.php
+++ membership/consolsummary.php
@@ -128,7 +128,7 @@ html.loading body {
 					 <select type="text" class="form-control" id="single"    placeholder="MemberID" name="single"  required>
 					<option value="" selected></option>
 						<?php 
-						$stmt12 = $conn->prepare("SELECT DISTINCT * FROM `tblretirementfunds1` ");
+						$stmt12 = $conn->prepare("SELECT DISTINCT * FROM `tblretirementfunds` ");
 						$stmt12->execute();
 						$result12 = $stmt12->get_result();
 						if ($result12->num_rows > 0) {
