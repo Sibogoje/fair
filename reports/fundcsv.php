70f51cacd4eccf1b64e5c517527e8d966022428b
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-27 11:45:02 +0530

    csv

--- reports/fundcsv.php
+++ reports/fundcsv.php
@@ -18,7 +18,7 @@ $query = $conn->query("SELECT
   `Amount`,
   `NewBalance`,
   `Comments`
-  FROM `tblMemberAccounts1` WHERE `fundid` ='$ii' AND DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC"); 
+  FROM `tblmemberaccounts` WHERE `fundid` ='$ii' AND DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC"); 
  
 if($query->num_rows > 0){ 
     $delimiter = ","; 
