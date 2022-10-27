edc89ccefcd2e601424c8539b5fd81087f708c4e
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-27 15:13:47 +0530

    y

--- files/index.php
+++ files/index.php
@@ -22,9 +22,9 @@ include 'db_connect.php' ?>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<!-- Select2 CSS --> 
 
-        <script src='select2/dist/js/select2.min.js' type='text/javascript'></script>
+        <script src='../select2/dist/js/select2.min.js' type='text/javascript'></script>
 
-        <link href='select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
+        <link href='../select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
 
   <!-- Favicons -->
   <link href="https://fair.liquag.com/" rel="icon">
