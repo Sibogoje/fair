e8f9ea2c93b9089e2bf7048c1513eb261ea8ef18
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-27 10:42:19 +0530

    DD

--- reports/fundreport1.php
+++ reports/fundreport1.php
@@ -10,7 +10,7 @@ if(count($_POST)>0){
 
 ////////////////////retrieve deceaced id
 
-$ttfundsresult = mysqli_query($conn, "SELECT COUNT(`memberID`) AS 'memberID' FROM `fundsums` WHERE `RetirementFundID` = '$id' AND DATE(`TransactionDate`) BETWEEN '$d1'  AND '$d2'  "); 
+$ttfundsresult = mysqli_query($conn, "SELECT COUNT(DISTINCT `memberID`) AS 'memberID' FROM `fundsums` WHERE `RetirementFundID` = '$id' AND DATE(`TransactionDate`) BETWEEN '$d1'  AND '$d2'  "); 
 $ttfundsrow = mysqli_fetch_assoc($ttfundsresult); 
 $ttfundmembers = $ttfundsrow['memberID'];
 
