1e53fbdbecb636b14df1acd5ca223db327ffa1f3
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-27 15:59:10 +0530

    S

--- files/index.php
+++ files/index.php
@@ -230,11 +230,7 @@ while($row = $result->fetch_assoc()) {
 
 <script src="../assets/vendor/php-email-form/validate.js"></script>
 
-
-
 <script src="../assets/js/main.js"></script>
-
-  
   <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
