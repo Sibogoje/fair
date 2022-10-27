a4dd54e4c07ec47fa6274517bead0bd439edd2bc
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-27 13:00:28 +0530

    6

--- files/index.php
+++ files/index.php
@@ -18,12 +18,16 @@ include 'db_connect.php' ?>
   <title>Files Management</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-	<!-- Select2 CSS --> 
-<script src='jquery-3.2.1.min.js' type='text/javascript'></script>
-        <script src='select2/dist/js/select2.min.js' type='text/javascript'></script>
 
-        <link href='../select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
+	<!-- Select2 CSS --> 
+
+    <script src=" https://code.jquery.com/jquery-3.5.1.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/b-2.2.3/b-html5-2.2.3/b-print-2.2.3/date-1.1.2/fh-3.2.4/r-2.3.0/rg-1.2.0/sc-2.0.7/sb-1.3.4/sp-2.0.2/sl-1.4.0/datatables.min.css"/>
+  <link href='select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/b-2.2.3/b-html5-2.2.3/b-print-2.2.3/date-1.1.2/fh-3.2.4/r-2.3.0/rg-1.2.0/sc-2.0.7/sb-1.3.4/sp-2.0.2/sl-1.4.0/datatables.min.js"></script>
+
+ <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+ <link href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css" rel="stylesheet">
 
   <!-- Favicons -->
   <link href="https://fair.liquag.com/" rel="icon">
@@ -158,15 +162,16 @@ include 'db_connect.php' ?>
         </main>
         
         
-          <!-- Vendor JS Files -->
-  <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/chart.js/chart.min.js"></script>
-  <script src="../assets/vendor/echarts/echarts.min.js"></script>
-  <script src="../assets/vendor/quill/quill.min.js"></script>
-  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
+        <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/php-email-form/validate.js"></script>
+<!-- Vendor JS Files -->
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 
   <!-- Template Main JS File -->
   <script src="../assets/js/main.js"></script>
