5a5a983fc86338b4a80bd9d329d5397da3224347
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-28 19:53:38 +0530

    fg

--- reports/scheduledreport.php
+++ reports/scheduledreport.php
@@ -266,6 +266,46 @@ html.loading body {
 
 
 
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/php-email-form/validate.js"></script>
+
+
+
+<script src="../assets/js/main.js"></script>
+    
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script>
+$(document).ready(function() {
+    $('#free').DataTable( {
+        lengthMenu: [
+            [10, 25, 50, -1],
+            [10, 25, 50, 'All'],
+        ],
+        dom: 'Blfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ],
+        responsive: true,
+        rowReorder: {
+            selector: 'td:nth-child(2)'
+        },
+       
+        
+    } );
+    
+  
+} );
+</script>
     <script src=""></script>
     <script src=""></script>
 
