392ac48b07661c9a3b7294e92c643620a6045a3b
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-28 17:20:07 +0530

    scheduleprocess

--- Transactions/scheduleprocess.php
+++ Transactions/scheduleprocess.php
@@ -2,51 +2,33 @@
 require_once '../scripts/connection.php';
 if(count($_POST)>0){
 $rr = "0";
-$val = 0.00;
+$val = 0;
+$null = NULL;
 /////////////retrieve all records that are not terminated ///////////////////////////////////
-$stmt = $conn->prepare("SELECT `MemberNo`, `FixedPaymentAmount`, `ApprovedBenefit`, `RetirementFundID` FROM `tblmembers1` WHERE `Terminated`=? AND `FixedPaymentAmount`>? ");
-$stmt->bind_param("ss", $rr, $val);
+$stmt = $conn->prepare("SELECT `memberID`, `TransactionPercent`, `FixedPaymentAmount`, `balance` FROM `member_fees` WHERE `FixedPaymentAmount`>? ");
+$stmt->bind_param("s", $val);
 $stmt->execute();
 $result = $stmt->get_result();
 if ($result->num_rows > 0) {
 while($row = $result->fetch_assoc()) {
-$MemberNo = $row['MemberNo'];
+$MemberNo = $row['memberID'];
 $update_date = date("Y-m-d");
-$PaymentDate = $update_date;
-$Details = "Regular Payment";
-$RetirementFundID = $row['RetirementFundID'];
-$FixedPaymentAmount = $row['FixedPaymentAmount'];	
-$ApprovedBenefit = $row['ApprovedBenefit'];
-$Comments = "";	
-$TransactionTypeID = 3;
-$Credit = 0;
+$FixedPaymentAmount = $row['FixedPaymentAmount'];
+$TransactionPercent = $row['TransactionPercent'];
+$prevbalance = $row['balance'];
 
-		$fetchtransactionrate = $conn->prepare("SELECT `TransactionPercent`, `AdminPercent` FROM `tblretirementfundfees1` WHERE RetirementFundID=? ");
-		$fetchtransactionrate->bind_param("s", $RetirementFundID);
-		$fetchtransactionrate->execute();
-		$rate = $fetchtransactionrate->get_result();
-		if ($rate->num_rows > 0) {
-		while($raterow = $rate->fetch_assoc()) {
-			
-		$transactionpercent = $raterow['TransactionPercent'];	
-		
-			
-			
-////////////check latest balance in transaction table
-$checkbalance = $conn->prepare("SELECT * FROM `qrybalance` WHERE memberID=?");
-$checkbalance->bind_param("s", $MemberNo);
-$checkbalance->execute();
-$balresult = $checkbalance->get_result();
-if ($balresult->num_rows > 0) {
-while($balrow = $balresult->fetch_assoc()) {
-$prevbalance = $balrow['NewBalance'];
-$accountsID = $balrow['accountsID'];	
+
+$Details = "Regular Payments";
+$Credit = "0";
+$Comments = "";
+
+$TransactionTypeID = 3;
 
 $newest = $prevbalance - $FixedPaymentAmount;
 
 //////insert account transaction from adhoc/////////////////////////////////////////////////////////////
 
-$insertnew = $conn->prepare("insert into `u747325399_fairlife`.`tblMemberAccounts1` (
+$insertnew = $conn->prepare("insert into `tblmemberaccounts` (
 
   `TransactionDate`,
   `TransactionTypeID`,
@@ -74,7 +56,7 @@ VALUES
 	?
   );");
 $insertnew->bind_param("sssssssss", 
-$PaymentDate, 
+$update_date, 
 $TransactionTypeID,
 $MemberNo,
 $Details,
@@ -84,268 +66,66 @@ $FixedPaymentAmount,
 $newest,
 $Comments
 );
-$insertnew->execute();
+
+if($insertnew->execute()){
 
 
-$transactionfee = $FixedPaymentAmount * ($transactionpercent/100);
-$newest1 = $newest - $transactionfee;
-$TransactionTypeID1 = 5;
-$Details2 = "Scheduled Transaction Fee";
+	$transactionfee = $FixedPaymentAmount * ($TransactionPercent/100);
+	$newest1 = $newest - $transactionfee;
+	$TransactionTypeID1 = 5;
+	$Details2 = "Scheduled Transaction Fee";
+	
+	
+	$insertnew->bind_param("sssssssss", 
+	$update_date, 
+	$TransactionTypeID1,
+	$MemberNo,
+	$Details2,
+	$Credit,
+	$newest,
+	$transactionfee,
+	$newest1,
+	$Comments
+	);
+	$insertnew->execute();
 
 
-$insertnew->bind_param("sssssssss", 
-$PaymentDate, 
-$TransactionTypeID1,
-$MemberNo,
-$Details2,
-$Credit,
-$newest,
-$transactionfee,
-$newest1,
-$Comments
-);
-$insertnew->execute();
-}
-}else{
-///// insert if there is no old transaction ///////////////////////////////////////////////
-$prebal = 0;	
-$TransactionTypeID = 3;
-$Credit = 0;
-$newest = $ApprovedBenefit - $FixedPaymentAmount;
-$latest = 0;
-$insertnew = $conn->prepare("insert into `u747325399_fairlife`.`tblMemberAccounts1` (
-
-  `TransactionDate`,
-  `TransactionTypeID`,
-  `memberID`,
-  `Details`,
-  `Credit`,
-  `StartingBalance`,
-  `Amount`,
-  `NewBalance`,
-  `Comments`
-)
-
-VALUES
-  (
-
-    ?,
-    ?,
-    ?,
-    ?,
-	?,
-	?,
-	?,
-	?,
-	?
-  );");
-$insertnew->bind_param("sssssssss", 
-$PaymentDate, 
-$TransactionTypeID,
-$MemberNo,
-$Details,
-$Credit,
-$ApprovedBenefit,
-$FixedPaymentAmount,
-$newest,
-$Comments
-
-);
-$insertnew->execute();	
-
-
-$transactionfee = $FixedPaymentAmount * ($transactionpercent/100);
-$newest1 = $newest - $transactionfee;
-$TransactionTypeID1 = 5;
-$Details2 = "Scheduled Transaction Fee";
-
-
-$insertnew->bind_param("sssssssss", 
-$PaymentDate, 
-$TransactionTypeID1,
-$MemberNo,
-$Details2,
-$Credit,
-$newest,
-$transactionfee,
-$newest1,
-$Comments
-);
-$insertnew->execute();
-
-}
-			
-			
-			
-}
-}else{
-	/*
-		$response = array(
-					'statusCode'=>201,
-					'datas'=>"Error: Fund Fees Not Found for ".$RetirementFundID
-					);
-				echo json_encode($response);
-				*/
-}
-}
-		$response = array(
-					'statusCode'=>200,
-					'datas'=>"Success: Transactions were successful"
-					);
-				echo json_encode($response);
 
 }else{
-		$response = array(
-					'statusCode'=>201,
-					'datas'=>"Error: No Members Found"
-					);
-				echo json_encode($response);
-}
-}else{
-		$response = array(
-					'statusCode'=>201,
-					'datas'=>"Error: Contact Administrator"
-					);
-				echo json_encode($response);
-}	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-		$fetchtransactionrate = $conn->prepare("SELECT `TransactionPercent` FROM `tblretirementfundfees1` WHERE RetirementFundID=? ");
-		$fetchtransactionrate->bind_param("s", $RetirementFundID);
-		$fetchtransactionrate->execute();
-		$rate = $fetchtransactionrate->get_result();
-		if ($rate->num_rows > 0) {
-		while($raterow = $rate->fetch_assoc()) {
-			
-			$transactionpercent = $raterow['TransactionPercent'];			
-			$transactionfee = $AdHocPayment * ($transactionpercent/100);	
-
-			
-			$newest1 = $newest - $transactionfee;
-			$TransactionTypeID = "5";
-			$Details = "Transaction Fee";
-			$latest = 1;
-			$insertnew = $conn->prepare("insert into `u747325399_fairlife`.`tblMemberAccounts1` (
-
-						  `TransactionDate`,
-						  `TransactionTypeID`,
-						  `memberID`,
-						  `Details`,
-						  `Credit`,
-						  `StartingBalance`,
-						  `Amount`,
-						  `NewBalance`,
-						  `Comments`,
-						  `latest`
-						)
-
-						VALUES
-						  (
-							?,
-							?,
-							?,
-							?,
-							?,
-							?,
-							?,
-							?,
-							?,
-							?
-						  );");
-						$insertnew->bind_param("ssssssssss", 
-						$PaymentDate, 
-						$TransactionTypeID,
-						$MemberID,
-						$Details,
-						$Credit,
-						$newest,
-						$transactionfee,
-						$newest1,
-						$Comments,
-						$latest
-						);
-						$insertnew->execute();
-						$update_date = date("d-m-Y");
-						$updaterunning = $conn->prepare("UPDATE `tblmembers1` SET `ruuning_balance`=?, `date_balance_updated`=? WHERE `MemberNo`=? ");
-						$updaterunning->bind_param("sss", $newest1, $update_date,  $MemberID);
-						$updaterunning->execute();
-						$response = array(
-					'statusCode'=>200,
-					'datas'=>$NewBalance
-					);
-				echo json_encode($response);
-						
-		}
-		}else{
-			$response = array(
-					'statusCode'=>201,
-					'datas'=>"Error: No fundfees linked to Fund"
-					);
-				echo json_encode($response);
-		}
-						
-	}
-	}else{
-		$response = array(
-					'statusCode'=>201,
-					'datas'=>"Error: No Fund linked to Deceased"
-					);
-				echo json_encode($response);
-	}
+	$response = array(
+		'statusCode'=>201,
+		'datas'=>"Error: Scheduled Payments were no Succesful"
+		);
+	echo json_encode($response);
 
 }
+
+
+
+
+}
+$response = array(
+	'statusCode'=>200,
+	'datas'=>"Success: Scheduled Payments were successful"
+	);
+echo json_encode($response);
+
+
 }else{
 	$response = array(
-					'statusCode'=>201,
-					'datas'=>"Error: No Deceased linked to Beneficiary"
-					);
-				echo json_encode($response);
+		'statusCode'=>201,
+		'datas'=>"Error: No Members Found"
+		);
+	echo json_encode($response);
 }
 
-
-///delete adhoc record////////////////////////////////////////////////////////////////////////
-$deleteadhoc = $conn->prepare("DELETE FROM `tbltempadhocpayments1` WHERE adhocPaymentID=? ");
-$deleteadhoc->bind_param("s", $adhocPaymentID);
-$deleteadhoc->execute();
-
-	
-}	
-}}
-	
 
 }else{
-	
-$response = array(
-					'statusCode'=>201,
-					'datas'=>$rr
-					);
-				echo json_encode($response);
+	$response = array(
+		'statusCode'=>201,
+		'datas'=>"Error: Error Occured, Please Contact Developer"
+		);
+	echo json_encode($response);
 }
-
-function transaction()
-
-
-
-{}
-*/
-?>
+?>			
