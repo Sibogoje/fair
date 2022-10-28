d4b4a4582df071154ab77bda50edae864e81a9a3
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-28 21:26:59 +0530

    kk

--- reports/scheduledreport.php
+++ reports/scheduledreport.php
@@ -374,7 +374,7 @@ alert("Please select fund to save report");
           var resp = $.trim(response);
           $("#gruu").html(resp);
 
-          datatable.ajax.reload();
+         // datatable.ajax.reload();
 
 
 
