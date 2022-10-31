27b818a81b36305f7e9240cd2d0d68ea1dd1888b
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-31 15:34:03 +0530

    HJ

--- membership/knew.php
+++ membership/knew.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <?php
 require_once '../scripts/connection.php';
 
@@ -64,6 +64,7 @@ $conn->close();
 
 
 ?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -77,7 +78,7 @@ $conn->close();
   <!-- Favicons -->
   <link href="https://fair.liquag.com/" rel="icon">
   <link href="https://fair.liquag.com/" rel="apple-touch-icon">
-
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -94,7 +95,7 @@ $conn->close();
   <!-- Template Main CSS File -->
   <link href="../assets/css/style.css" rel="stylesheet">
   
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.2.2
@@ -250,14 +251,7 @@ $conn->close();
 
   <script src="../assets/vendor/php-email-form/validate.js"></script>
   <!-- Vendor JS Files -->
-  <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/chart.js/chart.min.js"></script>
-  <script src="../assets/vendor/echarts/echarts.min.js"></script>
-  <script src="../assets/vendor/quill/quill.min.js"></script>
-  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
+
 
   <!-- Template Main JS File -->
   <script src="../assets/js/main.js"></script>
