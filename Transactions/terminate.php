f610481e8c5da9a8f42087099b03df9be829994c
Sibogoje <sibandzesiboniso0@gmail.com>
2022-10-31 17:08:22 +0530

    KK

--- Transactions/terminate.php
+++ Transactions/terminate.php
@@ -116,7 +116,7 @@ $conn->close();
   <meta content="" name="keywords">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<!-- Select2 CSS --> 
-<script src='jquery-3.2.1.min.js' type='text/javascript'></script>
+
         <script src='select2/dist/js/select2.min.js' type='text/javascript'></script>
 
         <link href='select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
