861309569d49aa2ec1f084253d01c2e67bb69be4
Sibogoje <sibandzesiboniso0@gmail.com>
2022-11-29 18:56:37 +0530

    XX

--- reports/adhoccsv.php
+++ reports/adhoccsv.php
@@ -14,10 +14,10 @@ $mntharray2 =  str_replace( array('[',']') , ''  , $mntharray1 );
 
 if (in_array("all", $mntharray)){
 
-   $choose = "`tblMemberAccounts1` WHERE TransactionTypeID = '4' AND  DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC, accountsID DESC";  
+   $choose = "`tblmemberaccounts` WHERE TransactionTypeID IN ('4') AND  DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC, accountsID DESC";  
 }else{
   
- $choose = "`tblMemberAccounts1` WHERE TransactionTypeID = '4' AND  `memberID` IN ({$mntharray2}) AND DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC, accountsID DESC";   
+ $choose = "`tblmemberaccounts` WHERE TransactionTypeID IN ('4') AND  `memberID` IN ({$mntharray2}) AND DATE(TransactionDate) BETWEEN '$d1'  AND '$d2'  ORDER BY TransactionDate DESC, accountsID DESC";   
 }
 
 
@@ -38,7 +38,7 @@ $query = $conn->query("SELECT
  
 if($query->num_rows > 0){ 
     $delimiter = ","; 
-    $filename = "Adhoc Report  ". date('Y-m-d') . ".csv"; 
+    $filename = "ADHOC REPORT  ". date('Y-m-d') . ".csv"; 
      
     // Create a file pointer 
     $f = fopen('php://memory', 'w'); 
@@ -50,6 +50,13 @@ if($query->num_rows > 0){
     // Output each row of the data, format line as csv and write to file pointer 
     while($row = $query->fetch_assoc()){ 
       //  $status = ($row['status'] == 1)?'Active':'Inactive'; 
+      $statement = $conn->prepare("SELECT MemberNo FROM `tblmembers` WHERE `memberID` = ?");
+      $statement->bind_param("s", $row['memberID']);
+							$statement->execute();
+							$result = $statement->get_result();
+							$rowB = $result->fetch_assoc();
+							$statement->close();
+							//$memberID = $rowB['MemberNo'];
      
 							if ($row['Credit'] == "1"){
 								$tyes = "Credit";
@@ -57,9 +64,10 @@ if($query->num_rows > 0){
 								$tyes = "Debit";
 								
 							}
-        $lineData = array($row['memberID'],$row['TransactionDate'], $row['Details'], $tyes, $row['Comments'], $row['StartingBalance'], $row['Amount'], $row['NewBalance']); 
+        $lineData = array($rowB['MemberNo'],$row['TransactionDate'], $row['Details'], $tyes, $row['Comments'], $row['StartingBalance'], $row['Amount'], $row['NewBalance']); 
         fputcsv($f, $lineData, $delimiter); 
     } 
+
 
     // Move back to beginning of file 
     fseek($f, 0); 
